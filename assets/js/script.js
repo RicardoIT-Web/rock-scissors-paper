c21289c6a9314dd7308a33c5cfd9d8793516534a
RicardoIT-Web <rick_piedade@hotmail.com>
2021-09-28 15:00:15 +0000

    add basic none changeable statements to JS code

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+/**
+ * Define fix statements that contain content that does not change
+ */
+
+const playerXdisplay = document.getElementsByClassName("playerXselection");
+const playerYdisplay = document.getElementsByClassName("playerYselection");
+const resultsDisplay = document.getElementById("result");
