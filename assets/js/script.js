1de5552cfda06ce312b16cb6b24818a2ecef0a0d
RicardoIT-Web <rick_piedade@hotmail.com>
2021-09-29 10:15:55 +0000

    conclude javascript basic setups and initiate README file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,71 @@
 /**
- * Define fix statements that contain content that does not change
+ * Define fix statements containing the content that do not change
  */
 
-const playerXdisplay = document.getElementsByClassName("playerXselection");
-const playerYdisplay = document.getElementsByClassName("playerYselection");
+const playerSelectionDisplay = document.getElementById("player-selection");
+const computerSelectionDisplay = document.getElementById("computer-selection");
 const resultsDisplay = document.getElementById("result");
+const availableSelections = document.querySelectorAll("button");
+
+let playerSelection
+let computerSelection
+
+
+/**
+ * add a click eventlistener to the available clickable options
+ */
+
+availableSelections.forEach(button => {
+    button.addEventListener("click", (event) => {
+    playerSelection = event.target.id
+    playerSelectionDisplay.innerHTML = playerSelection
+    generateComputerSelection()
+    getResult()
+    })
+})
+
+/**
+ * function to generate comupter selections
+ */
+
+function generateComputerSelection() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+        computerSelection = "Rock"
+    }
+    else if (randomNumber === 2) {
+        computerSelection = "Scissors"
+    }
+    else if (randomNumber === 3) {
+        computerSelection = "Paper"
+    }
+
+    computerSelectionDisplay.innerHTML = computerSelection
+}
+
+function getResult() {
+    if (computerSelection === playerSelection) {
+        result = "Its a Draw!"
+    }
+    else if (computerSelection === "Rock" && playerSelection ==="Paper") {
+        result = "You Win!"
+    }
+    else if (computerSelection === "Rock" && playerSelection ==="Scissors") {
+        result = "Computer Won!"
+    }
+    else if (computerSelection === "Scissors" && playerSelection ==="Paper") {
+        result = "Computer Won!"
+    }
+    else if (computerSelection === "Scissors" && playerSelection ==="Rock") {
+        result = "You Win!"
+    }
+    else if (computerSelection === "Paper" && playerSelection ==="Scissors") {
+        result = "You Win!"
+    }
+    else if (computerSelection === "Paper" && playerSelection ==="Rock") {
+        result = "Computer Won!"
+    }
+
+    resultsDisplay.innerHTML = result
+}
